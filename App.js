a039f2c8eb9a17d565a2fa3776d554d3917fa332
guimatiusso <rottamatiusso@gmail.com>
2023-01-26 20:48:24 -0300

    diagramacao tela inicial

--- App.js
+++ App.js
@@ -1,27 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Home } from "./src/Home/Home.js";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Home />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    fontSize: 30,
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
-});
