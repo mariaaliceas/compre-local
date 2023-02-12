f8bcd2fe683974d7cec0e0bc1945d36eb74ba491
DouglasKenjiSakakibara01 <douglaskenjisakakibara@gmail.com>
2023-02-12 14:43:12 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StyleSheet, Text, View } from 'react-native';
-import {startLogin} from '../comprelocal/src/startLogin';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {login} from '../comprelocal/src/login';
@@ -8,6 +7,62 @@ import {finalCadastro} from '../comprelocal/src/finalCadastro';
 import {cadastroComercio1} from '../comprelocal/src/cadastroComercio1';
 import {cadastroComercio2} from '../comprelocal/src/cadastroComercio2';
 const Stack = createNativeStackNavigator();
+const styles=StyleSheet.create({
+  container:{
+  display:'flex',
+  backgroundColor:'green',
+  flexDirection:'column',
+  alignItems:'center',
+  flex:1,
+  
+  },
+  
+  texto1:{
+  color:'white',
+  fontWeight: 'bold',
+  marginTop:100,
+  fontSize:30
+  },
+  
+  texto2:{
+  color:'white',
+  fontWeight: 'bold',
+  marginTop:15,
+  fontSize:20
+  
+  },
+  
+  containerBotao: {
+      display:'flex',
+      marginTop:20,
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center',
+      
+
+
+      
+      
+  
+  },
+  botao:{
+      width: 200,
+      padding:5,
+      marginBottom:20,
+      display:'flex',
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center',
+      backgroundColor:'white',
+      borderRadius:8,
+  
+  
+  },
+  textoBotao:{
+  color:'green',
+  
+  }
+});
 export default function App() {
   return (
       <>
@@ -32,8 +87,32 @@ export default function App() {
 
       </Stack.Navigator>
     </NavigationContainer>
+    <SafeAreaView style={styles.container}>   
+        
+        <Text style={styles.texto1}>BEM VINDO!</Text>
+        <Text style={styles.texto2}>Agradeçemos sua atitude de apoiar o comercio local</Text>  
+        <View style={styles.containerBotao}>
+            <TouchableOpacity style={styles.botao} 
+             onPress={() =>
+        navigation.navigate('cadastro')
+      }>
+                <Text  style={styles.textoBotao}>Criar Conta</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.botao}  
+            onPress={() =>
+        navigation.navigate('login')
+      } >
+                <Text  style={styles.textoBotao}>Fazer Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.botao} >
+                <Text  style={styles.textoBotao}>Continuar como Google</Text>
+            </TouchableOpacity>
+            
+        </View>
+        </SafeAreaView>
 
-    <startLogin></startLogin>
+
+    
       </>
     
   );
