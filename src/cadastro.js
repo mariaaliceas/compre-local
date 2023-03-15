45e1e9d689cca89a497b93bf01f959680ec483bd
Douglas Kenji Sakakibara <douglaskenjisakakibara@gmail.com>
2023-03-14 23:29:43 -0300

    continuando a integracao do frontend com o backend

--- src/cadastro.js
+++ src/cadastro.js
@@ -3,7 +3,7 @@ import React, { useState} from 'react';
 import { Alert,StyleSheet, Text, View,TouchableOpacity,TextInput} from 'react-native';
 import {login} from './login';
 import {finalCadastro} from './finalCadastro';
-
+import Axios from 'axios'
 
 
 
@@ -13,48 +13,73 @@ const cadastro=({navigation})=>{
     var telefone='';
     var senha='';
     const submit = () => {
+        /*
+        const enviar = {
+            "nome":nome,
+            "email":email,
+            "telefone":telefone,
+            "senha":senha,
+        };
+        */
+       
         const enviar = {
             nome,
             email,
             telefone,
-            senha,
-        };
-        fetch('http://localhost:3002/usuarioCadastro', {
-            method: 'POST',
+            senha
+        }
+        
+        fetch('http://192.168.1.11:3002/usuarioCadastro', {
+            method: 'GET',
+            
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(enviar),
+            
+            //body: JSON.stringify(enviar),
+            //body: enviar,
         })
+
+        //Axios.get("http://192.168.1.11:3002/usuarioCadastro")
         .then(async res => { 
             try {
                 const jsonRes = await res.json();
                 if (res.status !== 200) {
-                    console.log(jsonRes.message)
+                    //console.log(jsonRes.message)
                     Alert.alert('Erro no registro do cadastro','Tente novamente mais tarde')
                 } else {
                     
-                    console.log(jsonRes.message);
+                    //console.log(jsonRes.message);
+                    Alert.alert("Cadastro realizado")
                     return navigation.navigate('finalCadastro')
                 }
             } catch (err) {
-                console.log(err);
+                Alert.alert('Erro dentro do then')
+                //console.log(err);
             };
         })
         .catch(err => {
-            console.log(err);
+            Alert.alert('Erro fora do then')
+            //console.log(err);
         });
+        
+       
     };
 
     const criar=()=>{
 
     //Testando somente as condicoes de validacao
-     if((nome && telefone && email && senha && confirma)!=''){
+     if((nome && telefone && email && senha)!='' && (senha.length>7)){
         //Alert.alert('Cadastro realizado','Continue explorando o app')
         submit();
     }
-     else{
+    else if((nome || telefone || email || senha)==''){
+        
         Alert.alert('Erro no cadastro', 'Preencha os campos corretamente!')
+
+    }
+     else{
+        Alert.alert("Erro no cadastro", "Digite uma senha com pelo menos 8 caracteres")
     }
 }  
     return(
@@ -64,7 +89,7 @@ const cadastro=({navigation})=>{
         <TextInput style={styles.botao} placeholder='Nome Completo' onChangeText={text=>nome=text} />
         <TextInput style={styles.botao} placeholder='Telefone'  onChangeText={text=>telefone=text}/>
         <TextInput style={styles.botao} placeholder='Email'  onChangeText={text=>email=text}/>
-        <TextInput style={styles.botao} placeholder='Senha' secureTextEntry={true} onChangeText={text=>senha=text}/>
+        <TextInput style={styles.botao} placeholder='Senha(Com no mínimo 8 caracteres)' secureTextEntry={true} onChangeText={text=>senha=text}/>
         
        
 
@@ -116,7 +141,7 @@ const styles=StyleSheet.create({
     color:'white',
     backgroundColor:'gray',
     marginBottom:5,
-    width:250
+    width:260
     },
     
     botao2:{
@@ -136,108 +161,3 @@ const styles=StyleSheet.create({
 });
 
 export default cadastro;
-/* //Tentativa de fazer a validacao por campo para aparecer os campos que nao foram preenchidos corretamente
-import React from 'react';
-import {Alert, StyleSheet, Text, View,TouchableOpacity,TextInput} from 'react-native';
-import {login} from './login';
-import {finalCadastro} from './finalCadastro';
-
-var nome='';
-var email='';
-var telefone='';
-var senha='';
-var confirma='';
-
-
-const criar=({navigation})=>{
-    let erro=false;
-    if((nome && telefone && email && senha && confirma && (senha==confirma))!='' ){
-        return navigation.navigate('finalCadastro')
-    }
-    else{
-        
-    }
-
-       
-}
-
-const cadastro=({navigation})=>{
-    return(
-        <View style={styles.container}>
-        <Text style={styles.textoContainer}>Crie sua conta para começar</Text>
-        <View style={styles.containerBotao}>
-        <TextInput style={styles.botao} placeholder='Nome Completo' onChangeText={text=>nome=text} />
-        <TextInput style={styles.botao} placeholder='Telefone'  onChangeText={text=>telefone=text}/>
-        <TextInput style={styles.botao} placeholder='Email'  onChangeText={text=>email=text}/>
-        <TextInput style={styles.botao} placeholder='Senha' secureTextEntry={true} onChangeText={text=>senha=text}/>
-        <TextInput style={styles.botao} placeholder='Confirme sua senha' secureTextEntry={true} onChangeText={text=>confirma=text} />
-       
-
-        
-        <View style={styles.containerBotao2}>
-        <TouchableOpacity style={styles.botao2}  onPress={() =>criar()}>
-       
-            <Text style={styles.textoBotao2} >Criar conta</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botao2}  onPress={() =>
-        navigation.navigate('login')
-      }>
-            <Text style={styles.textoBotao2}>Já tem uma conta?</Text>
-        </TouchableOpacity>
-        </View>
-        </View>
-        </View>
-
-    );
-};
-const styles=StyleSheet.create({
-    container:{
-    backgroundColor:'green',
-    display:'flex',
-    flexDirection:'column',
-    alignItems:'center',
-    flex:1
-    },
-    textoContainer:{
-    marginTop:140,
-    marginBottom:15,
-    color:'white',
-    fontSize:25
-    },
-    
-    
-    
-    containerBotao2:{
-    marginTop:20,
-    display:'flex',
-    flexDirection:'row'
-    
-    },
-    
-    botao:{
-    padding:15,
-    borderRadius:5,
-    color:'white',
-    backgroundColor:'gray',
-    marginBottom:5,
-    width:250
-    },
-    
-    botao2:{
-    padding:15,
-    borderRadius:5,
-    color:'',
-    backgroundColor:'white',
-    marginBottom:5,
-    marginRight:20,
-    },
-    
-    
-    textoBotao2:{
-    color:'green',
-    
-    }
-});
-
-export default cadastro;
-*/
